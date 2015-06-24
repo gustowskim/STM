5e530667f27c9d1c8131699d093c98b9eb4d2be0
mgustowski <maciej.gustowski@yumasoft.pl>
2015-06-24 11:03:47 +0200

    Added translations

--- STM.Tests/TestExample.cs
+++ STM.Tests/TestExample.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using NUnit.Framework;
+using System.Threading;
+using System.Globalization;
 
 namespace STM.Tests
 {
@@ -9,6 +11,23 @@ namespace STM.Tests
         [Test]
         public void EmptyTest()
         {
+
+        }
+
+        [Test]
+        public void Translation_resurces_test()
+        {
+            var testStringPL = "TestString_PL";
+            var testStringDefault = "TestString_DEFAULT";
+            
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+            Assert.AreEqual(STM.Resources.Translations.Translation.Test_TestString, testStringDefault);
+
+            var ci = new CultureInfo("pl");
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentUICulture = ci;
+            Assert.AreEqual(STM.Resources.Translations.Translation.Test_TestString, testStringPL);
         }
     }
 }
