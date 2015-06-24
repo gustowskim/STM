8f70a1ea5a86424936d318d46a9f5765831e0cdb
mgustowski <maciej.gustowski@yumasoft.pl>
2015-06-24 07:53:01 +0200

    Added nunit test project

---
+++ STM.Tests/TestExample.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace STM.Tests
+{
+    [TestFixture]
+    public class TestExample
+    {
+        [Test]
+        public void EmptyTest()
+        {
+        }
+    }
+}
